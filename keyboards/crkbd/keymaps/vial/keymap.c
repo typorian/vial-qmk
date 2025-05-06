63c332d05e2d2e44ac2827ce3ee7e1a2a4620c04
typorian <eidorian@posteo.de>
2025-05-06 08:22:37 +0200

    try adding super alt tab

--- keyboards/crkbd/keymaps/vial/keymap.c
+++ keyboards/crkbd/keymaps/vial/keymap.c
@@ -388,3 +388,34 @@ bool oled_task_user(void) {
 }
 
 #endif
+
+bool is_alt_tab_active = false;
+enum custom_keycodes {          // Make sure have the awesome keycode ready
+  ALT_TAB = SAFE_RANGE,
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    if (is_alt_tab_active) {
+        unregister_code(KC_LALT);
+        is_alt_tab_active = false;
+    }
+    return state;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode){
+        case ALT_TAB: // super alt tab macro
+            if (record->event.pressed) {
+                if (!is_alt_tab_active) {
+                    is_alt_tab_active = true;
+                    register_code(KC_LALT);
+                }
+                register_code(KC_TAB);
+            } else {
+                unregister_code(KC_TAB);
+            }
+            break;
+        return false;
+     }
+  return true;
+}
