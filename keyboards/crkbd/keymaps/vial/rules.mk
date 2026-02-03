7e60fd6a5120e95615fa07f5c254b6b77108c6da
typorian <eidorian@posteo.de>
2026-02-03 22:05:54 +0100

    re-add other keyboards

---
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -0,0 +1,14 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+
+RGBLIGHT_ENABLE     = yes
+RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+MOUSEKEY_ENABLE     = no
+OLED_ENABLE         = yes
+OLED_DRIVER         = ssd1306
+EXTRAKEY_ENABLE     = yes
+COMBO_ENABLE        = no
+WPM_ENABLE			= yes
+
+QMK_SETTINGS        = yes
